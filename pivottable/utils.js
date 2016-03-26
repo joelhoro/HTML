4d24a65d8e9729682170ddc03c0f8531ba90c699
Joel <joel@horowitz.com>
2016-03-25 21:08:06 -0400

    Factoring out stuff in utils.js

---
+++ pivottable/utils.js
@@ -0,0 +1,23 @@
+// Generic array functions
+Array.prototype.sum = function(fn) { 
+      if ( fn == undefined )
+            fn = function(x) { return x; };
+      return this.map(fn).reduce(function(a,b) { return a+b; } ) 
+}
+Array.prototype.where = function(values) { return _.where(this,values); }
+Array.prototype.groupBy = function(fn) { return _.groupBy(this,fn); }
+Array.prototype.minus = function(remove) { return $(this).not(remove); }
+Array.prototype.toObject = function(fn) { 
+      var obj = {}; 
+      this.forEach(function(field) { obj[field] = fn(field); });
+      return obj;
+}
+function FieldExtractor(fieldName) { return function(obj) { return obj[fieldName]; } }
+function ObjectFn(obj) { return function(fieldName) { return obj[fieldName]; } }
+function ToObject(keys,values) {
+      return _.object(_.zip(keys,values));
+}
+
+function HTMLWrapper(tagStyle) {
+	return function(x) { return "<"+tagStyle+">"+x+"</"+tagStyle+">"; }
+}
